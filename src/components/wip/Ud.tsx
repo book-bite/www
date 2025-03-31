bbdf19f8026581131fa3367c32980c9c079e1bfa
Utkarsh Dwivedi <utkrsh.dev@gmail.com>
2025-03-31 20:41:56 +0530

    Temporarily hide content for maintenance

--- src/components/wip/Ud.tsx
+++ src/components/wip/Ud.tsx
@@ -2,18 +2,23 @@ import { motion } from "framer-motion";
 const Ud = () => {
   const text = Array(100).fill("under development")
   return (
-    <div className="h-[2.25vw] border-b border-[black] overflow-hidden py-2 whitespace-nowrap relative">
-        <motion.div className="flex space-x-8 text-lg font-bold" animate={{x: ["0%", "-500%"]}} transition={{repeat: Infinity, duration: 50, ease:"linear"}}>
-        {text.map((text, index) => (
-          <span 
-          key={index} 
-          className={`whitespace-nowrap ${index % 2 === 0 ? "font-bold" : "font-normal"}`}
-        >
-          {text}
-        </span>
-        ))}
-        </motion.div>
-    </div>
+    <div className="h-8 md:h-[2.25vw] border-b border-black overflow-hidden py-2 whitespace-nowrap relative">
+  <motion.div
+    className="flex space-x-8 text-sm md:text-lg font-bold"
+    animate={{ x: ["0%", "-500%"] }}
+    transition={{ repeat: Infinity, duration: 50, ease: "linear" }}
+  >
+    {text.map((text, index) => (
+      <span 
+        key={index} 
+        className={`whitespace-nowrap ${index % 2 === 0 ? "font-bold" : "font-normal"}`}
+      >
+        {text}
+      </span>
+    ))}
+  </motion.div>
+</div>
+
   )
 }
 
