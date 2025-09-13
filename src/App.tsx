ee0d147ae6d3632773ff66205726c8628adcab4c
Utkarsh Dwivedi <utkrsh.dev@gmail.com>
2025-09-13 22:27:00 +0530

    cleanup
    for design

--- src/App.tsx
+++ src/App.tsx
@@ -1,11 +1,11 @@
-import Wip from "./components/wip/Wip"
+
 
 
 function App() {
   return (
     <>
-    <div id='main' className='bg-[#EAEDE5] min-h-screen w-full flex flex-col'>
-      <Wip />
+    <div>
+      
     </div>
     </>
   )
